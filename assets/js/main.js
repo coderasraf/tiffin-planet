dd70495aa8a9a6cc279c21f7dd1b3f2545413a97
hassasraf <hassasraf@gmail.com>
2021-05-05 00:10:47 +0600

    Added some javascript code

--- assets/js/main.js
+++ assets/js/main.js
@@ -76,98 +76,48 @@ minus.forEach((min)=>{
 })
 
 // Javascript map api
-let map;
-
+	
+		
       function initMap() {
-        map = new google.maps.Map(document.getElementById("map"), {
-          center: { lat: 40.730610, lng: -73.935242 },
-          zoom: 14,
-          styles: [
-            { elementType: "geometry", stylers: [{ color: "#242f3e" }] },
-            {
-              elementType: "labels.text.stroke",
-              stylers: [{ color: "#242f3e" }],
-            },
-            {
-              elementType: "labels.text.fill",
-              stylers: [{ color: "#746855" }],
-            },
-            {
-              featureType: "administrative.locality",
-              elementType: "labels.text.fill",
-              stylers: [{ color: "#d59563" }],
-            },
-            {
-              featureType: "poi",
-              elementType: "labels.text.fill",
-              stylers: [{ color: "#d59563" }],
-            },
-            {
-              featureType: "poi.park",
-              elementType: "geometry",
-              stylers: [{ color: "#263c3f" }],
-            },
-            {
-              featureType: "poi.park",
-              elementType: "labels.text.fill",
-              stylers: [{ color: "#6b9a76" }],
-            },
-            {
-              featureType: "road",
-              elementType: "geometry",
-              stylers: [{ color: "#38414e" }],
-            },
-            {
-              featureType: "road",
-              elementType: "geometry.stroke",
-              stylers: [{ color: "#212a37" }],
-            },
-            {
-              featureType: "road",
-              elementType: "labels.text.fill",
-              stylers: [{ color: "#9ca5b3" }],
-            },
-            {
-              featureType: "road.highway",
-              elementType: "geometry",
-              stylers: [{ color: "#746855" }],
-            },
-            {
-              featureType: "road.highway",
-              elementType: "geometry.stroke",
-              stylers: [{ color: "#1f2835" }],
-            },
-            {
-              featureType: "road.highway",
-              elementType: "labels.text.fill",
-              stylers: [{ color: "#f3d19c" }],
-            },
-            {
-              featureType: "transit",
-              elementType: "geometry",
-              stylers: [{ color: "#2f3948" }],
-            },
-            {
-              featureType: "transit.station",
-              elementType: "labels.text.fill",
-              stylers: [{ color: "#d59563" }],
-            },
-            {
-              featureType: "water",
-              elementType: "geometry",
-              stylers: [{ color: "#17263c" }],
-            },
-            {
-              featureType: "water",
-              elementType: "labels.text.fill",
-              stylers: [{ color: "#515c6d" }],
-            },
-            {
-              featureType: "water",
-              elementType: "labels.text.stroke",
-              stylers: [{ color: "#17263c" }],
-            },
-          ],
-        });
+		var options ={
+			center: { lat: 42.3601, lng: -71.0589 },
+			zoom: 8
+		}
+        var map = new google.maps.Map(document.getElementById("map"), options);
 
-      }
+		addMarker({
+			coords:{lat:42.4668, lng: -70.9495},
+			iconImage: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
+			content: `<h2>Boston Shopping Mall</h2>`
+		})
+		addMarker({
+			coords:{lat: 42.3601, lng: -71.0589},
+			iconImage: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
+			content: `<h2>Boston Resturent</h2>`
+		})
+		// Add marker function
+		function addMarker(props){
+			var marker = new google.maps.Marker({
+				position:props.coords,
+				map: map,
+			})
+
+			// Check for cusotm icon
+			if(props.iconImage){
+				marker.setIcon(props.iconImage)
+			}
+
+			// Check location content
+			if(props.content){
+				var infoWindow = new google.maps.InfoWindow({
+					content: props.content
+				})
+				
+				marker.addListener("click", () => {
+					infoWindow.open(map, marker)
+				});
+			}
+			
+		}
+
+    }
