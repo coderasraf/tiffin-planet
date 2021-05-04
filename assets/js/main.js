a801de5220e7b9746c78d76c0566c12814dee98d
hassasraf <hassasraf@gmail.com>
2021-05-05 00:41:45 +0600

    Updated some javascript code

--- assets/js/main.js
+++ assets/js/main.js
@@ -76,8 +76,6 @@ minus.forEach((min)=>{
 })
 
 // Javascript map api
-	
-		
       function initMap() {
 		var options ={
 			center: { lat: 42.3601, lng: -71.0589 },
@@ -85,16 +83,35 @@ minus.forEach((min)=>{
 		}
         var map = new google.maps.Map(document.getElementById("map"), options);
 
-		addMarker({
-			coords:{lat:42.4668, lng: -70.9495},
-			iconImage: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
-			content: `<h2>Boston Shopping Mall</h2>`
+		// Adding marker after clicking location
+		google.maps.event.addListener(map, 'click', (event)=>{
+			addMarker({coords:event.latLng})
 		})
-		addMarker({
-			coords:{lat: 42.3601, lng: -71.0589},
-			iconImage: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
-			content: `<h2>Boston Resturent</h2>`
-		})
+
+		// Array of markers
+		var markers = [
+			{
+				coords:{lat:42.4668, lng: -70.9495},
+				iconImage: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
+				content: `<h2>Boston Shopping Mall</h2>`
+			},
+			{
+				coords:{lat: 42.3601, lng: -71.0589},
+				iconImage: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
+				content: `<h2>Boston Resturent</h2>`
+			},
+			{
+				coords:{lat: 25.761681, lng: -80.191788},
+				iconImage: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
+				content: `<h2>Relax Resturent, Miami</h2>`
+			}
+		]
+
+		// loop through all markers
+		for(var i= 0; i < markers.length; i++){
+			addMarker(markers[i])
+		}
+		
 		// Add marker function
 		function addMarker(props){
 			var marker = new google.maps.Marker({
